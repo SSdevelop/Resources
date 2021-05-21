60144f5c7ac234135e82fd69431532313aeb3a7a
unknown <mansikeshari1999@gmail.com>
2021-05-22 00:46:11 +0530

    updated header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+      <ul>
+            <li><a href='/'>HOME</a></li>
+            <li><a href='/'>ABOUT</a></li>
+            <li><a href='/'>LOGIN</a></li>
+            <li><a href='/'>SIGNUP</a></li>
+     </ul>
+    )
+}
+
+export default Header
