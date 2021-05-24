4296c31d391833683180348d3734b47432909992
unknown <mansikeshari1999@gmail.com>
2021-05-24 21:49:15 +0530

    updated header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 //import logo from './logo.svg';
 //import './App.css';
+import {BrowserRouter as router} from 'react-router-dom'
 import Header from './components/header/Header.jsx'
 import './components/header/Header.css'
 function App() {
   return (
-    <div className="App">
-     <Header />
+    <div classname='App'>
+    <router>
+    <Header />
+    </router>
     </div>
   );
 }
